e755f92146be0bba4e0705c4b78e79b41db693bf
Gui.js <68768795+Guilzx@users.noreply.github.com>
2023-09-30 17:55:59 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,10 @@ const elements = {
 
 async function fetchDiscordStatus() {
 	try {
+		const [lanyardResponse, lookupResponse] = await Promise.all([
+			fetch(`https://api.lanyard.rest/v1/users/`).then((response) =>
+				response.json()
+			),
 			fetch(`https://discordlookup.mesavirep.xyz/v1/user/788443073885110302`).then(
 				(response) => response.json()
 			),
